58b5c37b390c105f0b32c1b351b71a9cdfb54ee4
Dhruv1238 <dhruv4075@gmail.com>
2024-01-28 00:00:52 +0530

    feat: Mobile Nav Complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       colors: {
-        baseBlue: '#605BFF',
+        baseBlue: "#605BFF",
       },
       fontFamily: {
-        'sans': ['Montserrat', 'sans-serif'],
-        'lato': ['Lato', 'sans-serif'],
-      }
+        sans: ["Montserrat", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
