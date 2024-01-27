bad2fe3f168f82ed56304803242e79d31bc6a13f
Dhruv1238 <dhruv4075@gmail.com>
2024-01-28 02:32:19 +0530

    fix: Drawer Z index | assets managed

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,15 +1,15 @@
+import { Link } from "react-router-dom"
 import { Drawer } from "../components/Drawer"
 
 export const Upload = () => {
     return (
         <>
-            {/* <div className="md:hidden"> */}
+            <div className="flex h-screen w-screen items-center justify-center">
                 <Drawer />
-            {/* </div> */}
-            <div className="hidden md:flex">
-                <h1>Upload</h1>
+                <Link to='/' className="z-40 text-center items-center fixed">
+                    <h1 >Upload</h1>
+                </Link>
             </div>
-            
         </>
     )
 }
